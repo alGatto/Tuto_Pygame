fbd1dcf35b418091cb2d7eb368dfc911d8e3fb0c
alGatto <gattuso.al@gmail.com>
2015-11-11 20:02:40 +0100

    Image load, fill, blit, flip

--- tutoClass.py
+++ tutoClass.py
@@ -4,12 +4,17 @@ import pygame
 
 class Game(object):
     def main(selfself, screen):
+        image = pygame.image.load('ressources/Ducky-static.png') #charge l'image du personnage
         while 1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: # si la croix en haut à gauche de la fenêtre est cliquée
                     running = False # la boucle de jeu s'arrête
                 if event.type == pygame.KEYDOWN and event.key == pygame.ESCAPE: # si une touche plus la touche esc est préssée
                     running = False # la boucle de jeu s'arrête
+
+            screen.fill((200, 200, 200))
+            screen.blit(image, (320, 240))
+            pygame.display.flip()
 
 if __name__ == '__main__':
     pygame.init()
