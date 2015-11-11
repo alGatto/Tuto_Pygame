70e5d594ea59b7161f2b9ecb17f77cca2744dd43
alGatto <gattuso.al@gmail.com>
2015-11-11 22:14:36 +0100

    Gravity

--- player.py
+++ player.py
@@ -9,6 +9,9 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('ressources/Ducky-static.png') #charge l'image du personnage
         self.rect = pygame.rect.Rect((320,240), self.image.get_size())
 
+        self.resting = False
+        self.dy = 0 #delta y
+
     def update(self, dt, game):
         last = self.rect.copy() #copie de la position
 
@@ -17,13 +20,17 @@ class Player(pygame.sprite.Sprite):
             self.rect.x -= 300 * dt
         if key[pygame.K_RIGHT]: #si la touche flèche droite est appuyée
             self.rect.x += 300 * dt
-        if key[pygame.K_UP]: #si la touche flèche haut est appuyée
-            self.rect.y -= 300 * dt
-        if key[pygame.K_DOWN]: #si la touche flèche bas est appuyée
-            self.rect.y += 300 * dt
+
+        #Gravité
+        if self.resting and key[pygame.K_SPACE]:
+            self.dy = - 500
+        self.dy = min(400, self.dy + 40)
+
+        self.rect.y += self.dy * dt
 
         # Bloquer la sortie d'écran du perso
         new = self.rect
+        self.resting = False
         for cell in pygame.sprite.spritecollide(self, game.walls, False):
             cell = cell.rect
             if last.right <= cell.left and new.right > cell.left:
@@ -31,6 +38,9 @@ class Player(pygame.sprite.Sprite):
             if last.left >= cell.right and new.left < cell.right:
                 new.left = cell.right
             if last.bottom <= cell.top and new.bottom > cell.top:
+                self.resting = True
                 new.bottom = cell.top
+                self.dy = 0
             if last.top >= cell.bottom and new.top < cell.bottom:
                 new.top = cell.bottom
+                self.dy = 0
