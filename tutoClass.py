e07851af38c908f788adce39f31f9077de32634b
alGatto <gattuso.al@gmail.com>
2015-11-11 20:19:57 +0100

    User input

--- tutoClass.py
+++ tutoClass.py
@@ -15,8 +15,20 @@ class Game(object):
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: # si la croix en haut à gauche de la fenêtre est cliquée
                     running = False # la boucle de jeu s'arrête
+
                 if event.type == pygame.KEYDOWN and event.key == pygame.ESCAPE: # si une touche plus la touche esc est préssée
                     running = False # la boucle de jeu s'arrête
+
+            image_x += 10
+            key = pygame.key.get_pressed()
+            if key[pygame.K_LEFT]:
+                image_x -= 10
+            if key[pygame.K_RIGHT]:
+                image_x +=10
+            if key[pygame.K_UP]:
+                image_y -= 10
+            if key[pygame.K_DOWN]:
+                image_y +=10
 
             screen.fill((200, 200, 200))
             screen.blit(image, (image_x, image_y))
